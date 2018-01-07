6d99f21970cc961f235feb79439001c0896304ec
Paul Holden <pholden@stria.com>
2018-01-06 17:22:06 -0800

    SecurityGroup -> UserGroup

--- api/controllers/UserGroupController.js
+++ api/controllers/UserGroupController.js
@@ -1,5 +1,5 @@
 /**
- * SecurityGroupController
+ * UserGroupController
  *
  * @description :: Server-side actions for handling incoming requests.
  * @help        :: See https://sailsjs.com/docs/concepts/actions
@@ -16,25 +16,25 @@ module.exports = {
           if (!req.param('name')) {
             return callback({
               code: 'E_MISSING_PARAM',
-              message: 'Security group name is required'
+              message: 'User group name is required'
             });
           }
 
           callback();
         },
         (callback) => {
-          SecurityGroup.findOne({
+          UserGroup.findOne({
             account: req.session.account,
             name: req.param('name')
-          }, (err, securityGroup) => {
+          }, (err, userGroup) => {
             if (err) {
               return callback(err);
             }
 
-            if (securityGroup) {
+            if (userGroup) {
               return callback({
-                code: 'E_SECURITYGROUP_EXISTS',
-                message: 'A security group with that name already exists in this account'
+                code: 'E_USERGROUP_EXISTS',
+                message: 'A user group with that name already exists in this account'
               });
             }
 
@@ -42,29 +42,29 @@ module.exports = {
           });
         },
         (callback) => {
-          SecurityGroup.create({
+          UserGroup.create({
             account: req.session.account,
             name: req.param('name')
-          }).fetch().exec((err, securityGroup) => {
+          }).fetch().exec((err, userGroup) => {
             if (err) {
               return callback(err);
             }
 
-            callback(null, securityGroup);
+            callback(null, userGroup);
           });
         }
-      ], (err, securityGroup) => {
+      ], (err, userGroup) => {
         if (err) {
           return callback(err);
         }
 
-        callback(null, securityGroup);
+        callback(null, userGroup);
       })
     }).intercept('E_MISSING_PARAM', (err) => {
       return res.status(422).send({
         message: err.message
       })
-    }).intercept('E_SECURITYGROUP_EXISTS', (err) => {
+    }).intercept('E_USERGROUP_EXISTS', (err) => {
       return res.status(409).send({
         message: err.message
       });
@@ -72,8 +72,8 @@ module.exports = {
       return res.status(500).send({
         message: err.message
       });
-    }).exec((err, securityGroup) => {
-      res.status(201).send(securityGroup);
+    }).exec((err, userGroup) => {
+      res.status(201).send(userGroup);
     });
   }
 };
--- api/models/UserGroup.js
+++ api/models/UserGroup.js
@@ -1,5 +1,5 @@
 /**
- * SecurityGroup.js
+ * UserGroup.js
  *
  * @description :: A model definition.  Represents a database table/collection/etc.
  * @docs        :: https://sailsjs.com/docs/concepts/models-and-orm/models
