fb77bc2789e68313924e5cb9acabef803793b125
Paul Holden <pholden@stria.com>
2018-01-18 23:57:09 -0800

    Work on fixed connection constraints
    - Will lead to dedicated source terminals for workflow node outputs,
      execution flow, etc.

--- assets/js/workflow.js
+++ assets/js/workflow.js
@@ -18,15 +18,43 @@ $(document).ready(function () {
   new mxRubberband(graph);
 
   graph.setPanning(true);
+  graph.setConnectable(true);
   mxEvent.disableContextMenu(container);
 
   var parent = graph.getDefaultParent();
 
   graph.getModel().beginUpdate();
+
+  graph.getAllConnectionConstraints = function (terminal) {
+    if (terminal && this.model.isVertex(terminal.cell)) {
+      return [
+        new mxConnectionConstraint(new mxPoint(1, 0), false, 'Exec')
+      ];
+    }
+  }
+
+  mxConstraintHandler.prototype.intersects = function (icon, point, source, existingEdge) {
+    return (!source || existingEdge) || mxUtils.intersects(icon.bounds, point);
+  }
+
+  graph.connectionHandler.createEdgeState = function (me) {
+    var edge = graph.createEdge(null, null, null, null, null, 'edgeStyle=orthogonalEdgeStyle;curved=1');
+
+    return new mxCellState(this.graph.view, edge, this.graph.getCellStyle(edge));
+  }
+
+  graph.connectionHandler.isConnectableCell = function (cell) {
+    return false;
+  }
+
+  mxEdgeHandler.prototype.isConnectableCell = function (cell) {
+    return graph.connectionHandler.isConnectableCell(cell);
+  }
+
   try {
     var v1 = graph.insertVertex(parent, null, 'Hello', 20, 20, 80, 30);
     var v2 = graph.insertVertex(parent, null, 'World', 200, 150, 80, 30);
-    var e1 = graph.insertEdge(parent, null, '', v1, v2);
+
   } finally {
     graph.getModel().endUpdate();
   }
