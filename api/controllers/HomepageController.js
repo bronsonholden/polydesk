104434dd178a71316f763fda79bebfcd63789f6a
Paul Holden <pholden@stria.com>
2018-02-02 16:07:10 -0800

    Add controller for homepage

---
+++ api/controllers/HomepageController.js
@@ -0,0 +1,12 @@
+/**
+ * HomepageController
+ *
+ * @description :: Server-side actions for handling incoming requests.
+ * @help        :: See https://sailsjs.com/docs/concepts/actions
+ */
+
+module.exports = {
+  home: (req, res) => {
+    res.view('pages/homepage');
+  }
+};
