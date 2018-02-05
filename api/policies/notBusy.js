d64bd4cac3079406f9e325efb1c51577efa616e7
Paul Holden <pholden@stria.com>
2018-02-05 00:00:40 -0800

    Remove log message

--- api/policies/notBusy.js
+++ api/policies/notBusy.js
@@ -1,7 +1,6 @@
 const tooBusy = require('node-toobusy');
 
 module.exports = (req, res, next) => {
-  console.log(tooBusy.lag());
   if (tooBusy()) {
     res.status(503).send({
       message: 'The server is too busy'
