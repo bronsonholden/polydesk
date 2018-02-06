7a496a63b584428923d98caf826482a85e886cf2
Paul Holden <pholden@stria.com>
2018-02-06 14:07:16 -0800

    Add Polydesk config comments

--- config/polydesk.js
+++ config/polydesk.js
@@ -1,3 +1,10 @@
+/**
+ * Polydesk configuration
+ *
+ * General configuration for various Polydesk features.
+ */
+
 module.exports.polydesk = {
+  // The default number of seconds after which an activation instance expires
   defaultActivationExpires: 3600
 };
