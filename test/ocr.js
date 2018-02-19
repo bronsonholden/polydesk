0c371bc6d06abf9a57099bb3f656a5f486610673
Paul Holden <pholden@stria.com>
2018-02-18 18:57:11 -0800

    Additional gs options

--- test/ocr.js
+++ test/ocr.js
@@ -2,7 +2,7 @@ const exec = require('child_process').exec;
 
 var gs = 'gswin64'
 
-exec(`${gs} -sDEVICE=pngmonod -dDownScaleFactor=3 -r1200 -q -sPAPERSIZE=a4 -sOutputFile=p%03d.png ./test/test.pdf`, (err) => {
+exec(`${gs} -sDEVICE=pngmonod -dBATCH -dSAFER -dNOPAUSE -dDownScaleFactor=3 -r1200 -q -sPAPERSIZE=a4 -sOutputFile=p%03d.png ./test/test.pdf`, (err) => {
   console.log(err);
   process.exit(0);
 });
