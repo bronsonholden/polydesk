16fe40a060e2e944b5a43a8c6343d800b19d3b80
Paul Holden <pholden@stria.com>
2018-02-19 01:27:08 -0800

    Get rid of test dir and old code

--- test/ocr.js
+++
@@ -1,8 +0,0 @@
-const exec = require('child_process').exec;
-
-var gs = 'gswin64c'
-
-exec(`${gs} -sDEVICE=pngmonod -dBATCH -dSAFER -dNOPAUSE -dDownScaleFactor=3 -r1200 -q -sPAPERSIZE=a4 -sOutputFile=p%03d.png ./test/test.pdf`, (err) => {
-  console.log(err);
-  process.exit(0);
-});
