a64b181eeb1b638bbc3375763d25b705a3f05f97
Paul Holden <pholden@stria.com>
2018-02-18 18:58:25 -0800

    Use console version of gs

--- test/ocr.js
+++ test/ocr.js
@@ -1,6 +1,6 @@
 const exec = require('child_process').exec;
 
-var gs = 'gswin64'
+var gs = 'gswin64c'
 
 exec(`${gs} -sDEVICE=pngmonod -dBATCH -dSAFER -dNOPAUSE -dDownScaleFactor=3 -r1200 -q -sPAPERSIZE=a4 -sOutputFile=p%03d.png ./test/test.pdf`, (err) => {
   console.log(err);
