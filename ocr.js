4a03ebc99cdff4e78543aeb65ab1eb3cf1c8656c
Paul Holden <pholden@stria.com>
2018-02-20 00:51:54 -0800

    Write page images to temp dir

--- ocr.js
+++ ocr.js
@@ -1,6 +1,7 @@
 const fs = require('fs');
 const async = require('async');
 const readline = require('readline');
+const path = require('path');
 const exec = require('child_process').exec;
 const skipper = require('skipper-disk');
 
@@ -71,14 +72,21 @@ require('sails').load({
         const gs = process.platform === 'win32' ? 'gswin64c' : 'gs';
 
         async.eachSeries(pdfs, (pdf, callback) => {
-          exec(`${gs} -sDEVICE=pngmonod -dBATCH -dSAFER -dNOPAUSE -dDownScaleFactor=3 -r800 -q -sPAPERSIZE=a4 -sOutputFile=./documents/p%03d.png ${pdf}`, (err, stdin, stdout) => {
-            if (err) {
-              return callback(err);
-            }
+          async.waterfall([
+            (callback) => {
+              fs.mkdtemp(path.join(__dirname, '.tmp/ocr-'), callback);
+            },
+            (tmp, callback) => {
+              exec(`${gs} -sDEVICE=pngmonod -dBATCH -dSAFER -dNOPAUSE -dDownScaleFactor=3 -r800 -q -sPAPERSIZE=a4 -sOutputFile=${tmp}/p%03d.png ${pdf}`, (err, stdin, stdout) => {
+                if (err) {
+                  return callback(err);
+                }
 
-            sails.log.info('Converted ' + pdf + ' to pages');
-            fs.unlink(pdf, callback);
-          });
+                sails.log.info('Converted ' + pdf + ' to pages');
+                fs.unlink(pdf, callback);
+              });
+            }
+          ], callback);
         }, callback);
       },
       (callback) => {
