f6c769c5eee9badd062671b6c3e2c88d07c86191
Paul Holden <pholden@stria.com>
2018-03-18 23:17:23 -0700

    Update policies for DocumentController.upload
    - Add all capabilities when creating new user

--- config/policies.js
+++ config/policies.js
@@ -24,6 +24,7 @@ module.exports.policies = {
   },
   'DocumentController': {
     'browse': [ 'notBusy', 'isAuthenticated', 'canViewDocuments' ],
+    'upload': [ 'notBusy', 'isAuthenticated', 'canUploadDocuments' ],
     'view': [ 'notBusy', 'isAuthenticated', 'canViewDocuments' ],
     'addMetadataSet': [ 'notBusy', 'isAuthenticated', 'canAddDocumentMetadataSets' ],
     'updateMetadataSet': [ 'notBusy', 'isAuthenticated', 'canUpdateDocumentMetadataSets' ],
