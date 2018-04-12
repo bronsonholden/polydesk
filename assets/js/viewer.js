f0a32eee52cb9974946b41a65d2860948fb572ad
Paul Holden <pholden@stria.com>
2018-04-12 01:52:15 -0700

    Add custom viewer
    - Tore down almost all of the existing viewer, but will piece it back
      together using a custom viewer using the same style as the rest of the
      site.

---
+++ assets/js/viewer.js
@@ -0,0 +1,88 @@
+let pdfDocument;
+let PAGE_HEIGHT;
+const DEFAULT_SCALE = 1.33;
+
+pdfjsLib.workerSrc = './pdf.worker.js';
+pdfjsLib.getDocument('/documents/sample.pdf').then(pdf => {
+  pdfDocument = pdf;
+
+  let viewer = document.getElementById('viewer');
+  for (let i=0; i<pdf.pdfInfo.numPages; i++) {
+    let page = createEmptyPage(i+1);
+    viewer.appendChild(page);
+  }
+
+  loadPage(1).then(pdfPage => {
+    let viewport = pdfPage.getViewport(DEFAULT_SCALE);
+    PAGE_HEIGHT = viewport.height;
+    document.body.style.width = `${viewport.width}px`;
+  });
+});
+
+function createEmptyPage(num) {
+  let page = document.createElement('div');
+  let canvas = document.createElement('canvas');
+  let wrapper = document.createElement('div');
+  let textLayer = document.createElement('div');
+
+  page.className = 'page';
+  wrapper.className = 'canvasWrapper';
+  textLayer.className = 'textLayer';
+
+  page.setAttribute('id', `pageContainer${num}`);
+  page.setAttribute('data-loaded', 'false');
+  page.setAttribute('data-page-number', num);
+
+  canvas.setAttribute('id', `page${num}`);
+
+  page.appendChild(wrapper);
+  page.appendChild(textLayer);
+  wrapper.appendChild(canvas);
+
+  return page;
+}
+
+function loadPage(pageNum) {
+  return pdfDocument.getPage(pageNum).then(pdfPage => {
+    let page = document.getElementById(`pageContainer${pageNum}`);
+    let canvas = page.querySelector('canvas');
+    let wrapper = page.querySelector('.canvasWrapper');
+    let container = page.querySelector('.textLayer');
+    let canvasContext = canvas.getContext('2d');
+    let viewport = pdfPage.getViewport(DEFAULT_SCALE);
+
+    canvas.width = viewport.width * 2;
+    canvas.height = viewport.height * 2;
+    page.style.width = `${viewport.width}px`;
+    page.style.height = `${viewport.height}px`;
+    wrapper.style.width = `${viewport.width}px`;
+    wrapper.style.height = `${viewport.height}px`;
+    container.style.width = `${viewport.width}px`;
+    container.style.height = `${viewport.height}px`;
+
+    pdfPage.render({
+      canvasContext,
+      viewport
+    });
+
+    pdfPage.getTextContent().then(textContent => {
+      pdfjsLib.renderTextLayer({
+        textContent,
+        container,
+        viewport,
+        textDivs: []
+      });
+    });
+
+    page.setAttribute('data-loaded', 'true');
+
+    return pdfPage;
+  });
+}
+
+inView('.page').on('enter', function () {
+  setTimeout(function () {
+    loadPage($(this).attr('data-page-number'));
+  });
+  console.log($(this).attr)
+});
