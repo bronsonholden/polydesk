708d4066fb322230d1994de58bbc8e34092543bd
Paul Holden <pholden@stria.com>
2018-04-25 17:46:22 -0700

    Add migrate-arangodb project hook
    - Drops and recreates metadata ArangoDB database if migrate === drop,
      similar to other migrations.
    - alter and safe don't modify the database at all.

---
+++ api/hooks/migrate-arangodb/index.js
@@ -0,0 +1,43 @@
+/**
+ * ArangoDB migration hook
+ *
+ * Simple hook to migrate ArangoDB databases similarly to Sails migrations:
+ *   - alter
+ *   - safe
+ *   - drop
+ *
+ * alter and safe simply leave the databases alone, while drop will delete
+ * the database (if it exists) and create a fresh one.
+ *
+ * Like Sails, databases are NEVER dropped in the production environment.
+ */
+
+const arangoDb = require('arangojs');
+
+module.exports = (sails) => {
+  return {
+    initialize: (callback) => {
+      if (process.env.NODE_ENV === 'production' || _.get(sails, 'config.metadata.migrate') !== 'drop') {
+        return callback();
+      }
+
+      sails.log.info(`·• ArangoDB ${sails.config.metadata.arangoDb.url} Auto-migration...  (drop)`);
+
+
+      var db = new arangoDb.Database({
+        url: sails.config.metadata.arangoDb.url
+      });
+
+      db.useBasicAuth(sails.config.metadata.arangoDb.username, sails.config.metadata.arangoDb.password);
+      db.listDatabases().then((databases) => {
+        if (databases.filter(db => db !== '_system').indexOf(sails.config.metadata.arangoDb.database) > -1) {
+          return db.dropDatabase(sails.config.metadata.arangoDb.database);
+        }
+      }).then(() => {
+        return db.createDatabase(sails.config.metadata.arangoDb.database);
+      }).then(() => {
+        sails.log.info(` ✓ s3://${sails.config.documents.s3.bucket} Auto-migration complete.`);
+      }).then(callback).catch(callback);
+    }
+  };
+};
