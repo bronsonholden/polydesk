886061019450ee9c7dda5866fa06765a3b5134a9
Paul Holden <pholden@stria.com>
2018-08-05 23:55:34 -0700

    Fix spacing issue

--- api/hooks/data-loader/index.js
+++ api/hooks/data-loader/index.js
@@ -3,29 +3,37 @@
  */
 
 
- const _ = require('lodash');
- const arangoDb = require('arangojs');
- const fs = require('fs');
- const loadModels = require('./load-models');
- const loadArango = require('./load-arango');
+const _ = require('lodash');
+const arangoDb = require('arangojs');
+const fs = require('fs');
+const loadModels = require('./load-models');
+const loadArango = require('./load-arango');
 
- module.exports = (sails) => {
-   return {
-     initialize: (callback) => {
-       if (process.env.NODE_ENV === 'production' || !_.get(sails, 'config.polydesk.generateSampleData')) {
-         return callback();
-       }
+module.exports = (sails) => {
+  return {
+    initialize: (callback) => {
+      if (process.env.NODE_ENV === 'production' || !_.get(sails, 'config.polydesk.generateSampleData')) {
+        return callback();
+      }
 
-       sails.after([
-         'hook:orm:loaded',
-         'hook:migrate-arangodb:loaded',
-         'hook:dev-sample-data:loaded'
-       ], () => {
-         async.waterfall([
-           loadModels,
-           loadArango
-         ], callback);
-       });
-     }
-   };
- };
+      sails.log.info(`·• data-loader: Loading sample data...`);
+
+      sails.after([
+        'hook:orm:loaded',
+        'hook:migrate-arangodb:loaded',
+        'hook:dev-sample-data:loaded'
+      ], () => {
+        async.waterfall([
+          loadModels,
+          loadArango
+        ], (err) => {
+          if (err) {
+            return callback(err);
+          }
+
+          sails.log.info(`·• data-loader: Done!`);
+        });
+      });
+    }
+  };
+};
